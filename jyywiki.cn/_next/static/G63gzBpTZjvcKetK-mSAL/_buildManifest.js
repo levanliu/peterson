e91c3712746342ddd0f24e42495031db65f70680
levanliu <839119912@qq.com>
2024-01-25 05:07:31 +0000

    add

---
+++ jyywiki.cn/_next/static/G63gzBpTZjvcKetK-mSAL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],"/[[...index]]":["static/chunks/pages/[[...index]]-28ef7957fcd35a0c.js"],sortedPages:["/_app","/_error","/[[...index]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
